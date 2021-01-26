dda69164a619e2717637b6dfffaa5236c5a3f884
Rebecca Chen <rebecca061996@gmail.com>
2021-01-26 16:03:04 +0000

    modify code to use .value to update counter number

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
     const count = $(this).val().length;
     // use jQuery to traverse DOM tree to update counter
     const counter = $(this).next().children()[1];
-    counter.innerHTML = 140 - count;
+    counter.value = 140 - count;
     if (140 - count < 0) {
       $(counter).addClass('counter-negative');
     } else {
