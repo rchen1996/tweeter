71f9b6ca51af7f678c3685b8e919ccc7091264a3
Rebecca Chen <rebecca061996@gmail.com>
2021-01-27 20:42:00 +0000

    add functionality for to-top button to appear only when scrolled and bring user back to top and show/focus on the new tweet form

--- public/scripts/composer-buttons.js
+++ public/scripts/composer-buttons.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
+  // hide/show new tweet box
   $('#compose').click(function() {
     $('.new-tweet').slideToggle(300, function() {
       $('.new-tweet').addClass('new-tweet-show');
       $('#tweet-text').focus().select();
     });
+  });
+
+  // scroll to top & make form appear
+  $('#to-top').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, 600);
+    // check to see if form is already shown
+    if (!$('.new-tweet').show()) {
+      $('#compose').click();
+    } else {
+      $('#tweet-text').focus().select();
+    }
+  });
+
+  // make to top button appear only when scrolled
+  $(window).scroll(function() {
+    // scrollTop = 0 means already at top of page, so truthy means not at top
+    if ($(this).scrollTop()) {
+      $('#to-top').fadeIn();
+      $('#compose').fadeOut();
+    } else {
+      $('#to-top').fadeOut();
+      $('#compose').fadeIn();
+    }
   });
 });
