7a0e11f11293fb3b17449ac980e0e2ff36ec4ab9
Rebecca Chen <rebecca061996@gmail.com>
2021-01-27 19:58:46 +0000

    add code to handle button clicks that toggle the new-tweet container

---
+++ public/scripts/composer-buttons.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#compose').click(function() {
+    $('.new-tweet').slideToggle(300, function() {
+      $('.new-tweet').addClass('new-tweet-show');
+      $('#tweet-text').focus().select();
+    });
+  });
+});
