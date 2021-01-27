449fbd0bcba1632ef728fbaf81da34a9a70e9808
Rebecca Chen <rebecca061996@gmail.com>
2021-01-27 19:45:40 +0000

    remove console.log

--- public/scripts/compose-button.js
+++ public/scripts/compose-button.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
   $('#compose').click(function() {
-    console.log('clicked');
     $('.new-tweet').slideToggle(300, function() {
       $('.new-tweet').addClass('new-tweet-show');
     });
