cb02292667d73f7e9f9bc5d4cc1b6df276bd2490
Rebecca Chen <rebecca061996@gmail.com>
2021-01-27 19:48:29 +0000

    add functionality to automatically select text area when toggled

--- public/scripts/compose-button.js
+++ public/scripts/compose-button.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $('#compose').click(function() {
     $('.new-tweet').slideToggle(300, function() {
       $('.new-tweet').addClass('new-tweet-show');
+      $('#tweet-text').focus().select();
     });
   });
 });
